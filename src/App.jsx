393c1acada8cc43f8c749e6adf15bc9becfb0147
Megan McNeill <mcneim2@gmail.com>
2024-08-29 16:25:17 -0400

    updated css and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <div className="bg-reseda-green p-4" >
+      <div className="p-4" >
         <h1 className="text-3xl font-serif">Hello world!</h1>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,24 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM from "react-dom/client";
 
-import App from './App.jsx'
-import './App.css'
-
-
+import App from "./App.jsx";
+import "./App.css";
 
 // Bringing in the required imports from 'react-router-dom' to set up application routing behavior
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './index.css'
+import "./index.css";
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
 
 // Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-])
-
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 // Render the RouterProvider component
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
-)
+);
