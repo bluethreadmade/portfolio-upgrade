e5983d83c6ed22c8224a1708ae98784e0fbef775
Megan McNeill <mcneim2@gmail.com>
2024-08-29 14:00:30 -0400

    added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ export default {
       'sans': ['Arimo'],
       'serif': ['"IBM  Plex Sans"'],
     },
+    colors: {
+      'beige':'#F1EFEB',
+      'bright':'#FF715B',
+      'french-gray':'#BFCCD8',
+      'reseda-green':'#798E7B',
+      'gunmetal':'#143642'
+    },
     extend: {},
   },
   plugins: [],
