32782dae945df67c3e9e18169b2136c4bee62078
Megan McNeill <mcneim2@gmail.com>
2024-08-30 13:51:37 -0400

    refactor header responsive

--- src/components/Header1.jsx
+++ src/components/Header1.jsx
@@ -1,9 +1,14 @@
 export default function Header1() {
   return (
-    <header className="font-sans color-gunmetal text-base lg:text-xl flex">
-      <ul className="w-full flex flex-row justify-center sm:justify-between py-2 px-4 lg:px-24">
-        <li className="hidden lg:block">Megan McNeill</li>
-        <li>MeganMcNeill.dev</li>
+    <header className="relative items-center font-sans color-gunmetal text-base lg:text-xl flex py-2 px-4 lg:px-24 h-16">
+      <ul className="absolute left-0 flex items-center space-x-4">
+        <li className="hidden lg:block">MeganMcNeill.dev</li>
+      </ul>
+      {/* default center (absolute?) at small become left */}
+      <div className="inset-0 flex items-center justify-center absolute lg:absolute sm:relative md:relative">
+        <span>Megan McNeill</span>
+      </div>
+      <ul className="absolute right-0 flex items-center space-x-4">
         <li className="hidden sm:block">Megan.E.McNeill@gmail.com</li>
       </ul>
     </header>
