48ad3ee1b8660ac87ae7281065ffdfeeb3a84122
Megan McNeill <mcneim2@gmail.com>
2024-08-29 20:20:13 -0400

    create components and pages directories, created Header.jsx, updated App.jsx to pull header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <>
       <div className="p-4" >
+        <Header />
         <h1 className="text-3xl font-serif">Hello world!</h1>
       </div>
-    </>
   );
 }
 
