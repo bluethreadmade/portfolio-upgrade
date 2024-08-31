23c3a1cccf5b597294e93f02222ca3b4d7d0e58f
Megan McNeill <mcneim2@gmail.com>
2024-08-30 21:32:13 -0400

    set main popup to absolute, bottom with bottom padding

--- src/components/MainPopupButton.jsx
+++ src/components/MainPopupButton.jsx
@@ -2,7 +2,7 @@ export default function MainPopupButton() {
   return (
     <div
       id="buttonGroup"
-      className="flex flex-row w-full self-center max-w-sm border rounded-xl bg-bright p-1 justify-evenly divide-x font-sans color-gunmetal text-xl " 
+      className="flex flex-row w-full self-center absolute bottom-6 max-w-sm border rounded-xl bg-bright p-1 justify-evenly divide-x font-sans color-gunmetal text-xl " 
     >
       <p className="flex-1 text-center text-base lg:text-xl">Resume</p>
       <p className="flex-1 text-center text-base lg:text-xl">Contact</p>
